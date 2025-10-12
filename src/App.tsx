7fd41023b2f5dd84412ce1e562bca9f778c8bfe6
Goktug Mert Ozdogan <goktug.ozdogan@bahcesehir.edu.tr>
2025-10-12 19:05:11 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-brand text-white text-3xl font-semibold">
+      <>
+        Welcome to gokmeroz's portfolio 🚀. Here you can find everything about
+        Goktug Mert Ozdogan's career.
+      </>
+      <a href="https://x.com/gokmeroz_dev" style={{ color: "red" }}>
+        Follow me on Twitter/X!
+      </a>
+    </div>
+  );
+}
